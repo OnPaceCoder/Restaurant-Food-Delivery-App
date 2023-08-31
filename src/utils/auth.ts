43523da971adc29526d827293996640db3372250
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-08-31 23:23:57 +0530

    Add: Google auth and database connect

---
+++ src/utils/auth.ts
@@ -0,0 +1,16 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ token, session }) {
+      return session;
+    },
+  },
+};
