7a7db958a08bca9a3f7924fd1eb541cf7e08132a
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-09-04 00:55:45 +0530

    Add: APIs - Auth, Category, Orders , Products

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,19 @@
+import { prisma } from "@/utils/connect";
+import { NextResponse } from "next/server";
+
+// Fetch all categories
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany({
+      orderBy: {
+        img: "desc",
+      },
+    });
+    return new NextResponse(JSON.stringify(categories), { status: 200 });
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
+};
