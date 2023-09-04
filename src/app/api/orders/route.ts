c810fe6352c0f4e5f25ff81164ddead3d0095a34
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-09-04 23:48:07 +0530

    Add: Orders API

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -32,3 +32,25 @@ export const GET = async (req: NextRequest) => {
     );
   }
 };
+
+//Create Order
+export const POST = async (req: NextRequest) => {
+  const session = await getAuthSession();
+
+  if (session) {
+    try {
+      const body = await req.json();
+      const order = await prisma.order.create({
+        data: body,
+      });
+
+      return new NextResponse(JSON.stringify(order), { status: 201 });
+    } catch (error) {
+      console.log(error);
+      return new NextResponse(
+        JSON.stringify({ message: "Something went wrong!" }),
+        { status: 401 }
+      );
+    }
+  }
+};
