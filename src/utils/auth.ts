d07cfb0d3641871d62a0d192d9417ccf972b8634
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-09-04 23:49:06 +0530

    ADD: session and jwt added with isAdmin feature

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,27 @@
-import { NextAuthOptions } from "next-auth";
+import { NextAuthOptions, User, getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "./connect";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+declare module "next-auth" {
+  interface Session {
+    user: User & {
+      isAdmin: Boolean;
+    };
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    isAdmin: Boolean;
+  }
+}
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
@@ -10,7 +30,23 @@ export const authOptions: NextAuthOptions = {
   ],
   callbacks: {
     async session({ token, session }) {
+      if (token) {
+        session.user.isAdmin = token.isAdmin;
+      }
       return session;
+    },
+    async jwt({ token }) {
+      const userInDb = await prisma.user.findUnique({
+        where: {
+          email: token.email!,
+        },
+      });
+
+      token.isAdmin = userInDb?.isAdmin!;
+
+      return token;
     },
   },
 };
+
+export const getAuthSession = () => getServerSession(authOptions);
