b8ba0b922def351cadf7a0dd3a1a1d2fecfa5bbb
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-09-05 20:40:45 +0530

    Add: React-toastify and QueryProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
-import Notification from "@/components";
+import { Notification, Navbar, Footer } from "@/components";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
+import AuthProvider from "@/components/AuthProvider";
+import { QueryProvider } from "@/components/QueryProvider";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,8 +21,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Notification />
-        {children}
+        <AuthProvider>
+          <QueryProvider>
+            <Notification />
+            <Navbar />
+            {children}
+            <Footer />
+            <ToastContainer
+              position="bottom-right"
+              theme="dark"
+              autoClose={3000}
+            />
+          </QueryProvider>
+        </AuthProvider>
       </body>
     </html>
   );
