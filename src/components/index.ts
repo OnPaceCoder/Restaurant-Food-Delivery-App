09767626f08b9388d3e05bacfcb1efcda4f74969
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-09-06 23:54:45 +0530

    Add: New Components added

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,22 @@
 import Notification from "./Notification";
-
-export default Notification;
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import UserLinks from "./UserLinks";
+import CartIcon from "./CartIcon";
+import Menu from "./Menu";
+import Slider from "./Slider";
+import Featured from "./Featured";
+import Offer from "./Offer";
+import CountDown from "./CountDown";
+export {
+  Notification,
+  Footer,
+  Navbar,
+  UserLinks,
+  CartIcon,
+  Menu,
+  Slider,
+  Featured,
+  Offer,
+  CountDown,
+};
