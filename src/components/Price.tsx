5d65d941306df3caee9faa41178da2ced3d39a42
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-09-06 23:55:27 +0530

    Update: HandleCart feature added

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,30 +1,53 @@
 "use client";
 
+import { ProductTypes } from "@/types/types";
+import { useCartStore } from "@/utils/store";
 import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
-type Props = {
-  price: number;
-  id: number;
-  options?: { title: string; additionalPrice: number }[];
-};
-
-const Price = ({ price, id, options }: Props) => {
+const Price = ({ product }: { product: ProductTypes }) => {
   const [selected, setSelected] = useState(0);
-  const [total, setTotal] = useState(0);
+  const [total, setTotal] = useState(product.price);
   const [quantity, setQuantity] = useState(1);
 
+  const { addToCart } = useCartStore();
   useEffect(() => {
-    if (options?.length) {
-      setTotal(quantity * (price + options[selected].additionalPrice));
+    useCartStore.persist.rehydrate();
+  }, []);
+
+  useEffect(() => {
+    if (product.options?.length) {
+      setTotal(
+        quantity *
+          (Number(product.price) +
+            Number(product.options[selected].additionalPrice))
+      );
+    } else {
+      setTotal(quantity * Number(product.price));
     }
-  }, [quantity, selected]);
+  }, [quantity, selected, product]);
+
+  const handleCart = () => {
+    addToCart({
+      id: product.id,
+      title: product.title,
+      img: product.img,
+      price: total,
+      ...(product.options?.length && {
+        optionTitle: product.options[selected].title,
+      }),
+
+      quantity: quantity,
+    });
+    toast.success("The Product added to cart ");
+  };
 
   return (
     <div className="flex flex-col gap-4">
-      <h2 className="text-2xl font-bold">${total.toFixed(2)}</h2>
+      <h2 className="text-2xl font-bold">${Number(total).toFixed(2)}</h2>
       <div className="flex gap-4">
-        {options?.length &&
-          options?.map((option, index) => (
+        {product.options?.length! > 0 &&
+          product.options?.map((option, index) => (
             <button
               key={option.title}
               className="min-w-[6rem] p-2 ring-1 ring-red-400 rounded-md"
@@ -57,7 +80,10 @@ const Price = ({ price, id, options }: Props) => {
           </div>
         </div>
         {/* Cart button */}
-        <button className="uppercase w-56 bg-red-500 text-white p-3 ring-1 ring-red-500">
+        <button
+          className="uppercase w-56 bg-red-500 text-white p-3 ring-1 ring-red-500"
+          onClick={handleCart}
+        >
           Add to cart
         </button>
       </div>
